0a2efa1243eb70cee49e81dcf1269bc95262109d
Rafael García <rafaelgarcia1985@hotmail.com>
2026-08-16 15:21:52 +0200

    fix: refresh offline app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "trip-list-v2";
+const CACHE = "trip-list-v3";
 const ASSETS = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
