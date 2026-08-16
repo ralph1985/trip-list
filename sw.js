bd08c70206ad8ff1be240c95c76eaa19a79bad78
Rafael García <rafaelgarcia1985@hotmail.com>
2026-08-16 14:05:54 +0200

    feat: create TripList offline checklist

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE = "trip-list-v1";
+const ASSETS = ["/", "/index.html", "/styles.css", "/app.js", "/manifest.webmanifest", "/icon.svg"];
+
+self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
+self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
+self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
